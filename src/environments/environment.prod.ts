50484f4f0997486c2d8002e17b4d09365ed4db8c
Gabriel Yan Mitoso <gymitoso@gmail.com>
2018-08-26 17:26:03 -0400

    Change prod url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   api_url: 'https://bibliapp.herokuapp.com/api',
-  middleware_url: 'https://localhost:8080/api',
+  middleware_url: 'http://localhost:8080/api',
   production: true
 };
